b79df93b0b3e26d4abf65cbb42be979750e9a645
glbdmn <gabrieledamiani@hotmail.it>
2023-06-01 16:18:46 +0200

    aggiunto le img e stilizzato le card

--- index.php
+++ index.php
@@ -25,10 +25,10 @@ $collare = new Accessorio('2', '20€', 'collare');
 
 // istanza gioco
 
-$pallina_spugna = new giochi('1', '6€', 'pallina in spugna', 'cane');
-$peluque = new giochi('1', '6€', 'peluque', 'cane');
-$gomitolo = new giochi('1', '6€', 'gomitolo', 'gatto');
-$topolino = new giochi('1', '6€', 'topolino', 'gatto');
+$pallina_spugna = new giochi('1', '6€', 'pallina in spugna', 'cani');
+$peluque = new giochi('1', '6€', 'peluque', 'cani');
+$gomitolo = new giochi('1', '6€', 'gomitolo', 'gatti');
+$topolino = new giochi('1', '6€', 'topolino', 'gatti');
 // var_dump( $pallina_spugna );
 
 
@@ -49,30 +49,34 @@ $topolino = new giochi('1', '6€', 'topolino', 'gatto');
 
 <main>
     <div class="card">
-       <h2> utente: <?php echo  $gabriele->nome; ?> <?php echo  $gabriele->cognome; ?> </h2>
-       <h2> prodotto per: <?php echo  $pallina_spugna->nome_animale; ?></h2>
-       <h3> gioco: <?php echo  $pallina_spugna->nome_gioco; ?> </h3>
-       <h3> costo: <?php echo  $pallina_spugna->prezzo; ?> </h3>
+       <img class="two" src="img/palline.jpg" alt="">
+       <h2> Utente: <?php echo  $gabriele->nome; ?> <?php echo  $gabriele->cognome; ?> </h2>
+       <h2> Prodotti per: <?php echo  $pallina_spugna->nome_animale; ?></h2>
+       <h3> Gioco: <?php echo  $pallina_spugna->nome_gioco; ?> </h3>
+       <h3> Costo: <?php echo  $pallina_spugna->prezzo; ?> </h3>
     </div>
     <div class="card">
-       <h2> utente: <?php echo  $francesco->nome; ?> <?php echo  $francesco->cognome; ?> </h2>
-       <h2> prodotto per: <?php echo  $gomitolo->nome_animale; ?></h2>
-       <h3> gioco: <?php echo  $gomitolo->nome_gioco; ?> </h3>
-       <h3> costo: <?php echo  $gomitolo->prezzo; ?> </h3>
+       <img src="img/gomitolo.jpg" alt="">
+       <h2> Utente: <?php echo  $francesco->nome; ?> <?php echo  $francesco->cognome; ?> </h2>
+       <h2> Prodotto per: <?php echo  $gomitolo->nome_animale; ?></h2>
+       <h3> Gioco: <?php echo  $gomitolo->nome_gioco; ?> </h3>
+       <h3> Costo: <?php echo  $gomitolo->prezzo; ?> </h3>
 
     </div>
     <div class="card">
-       <h2> utente: <?php echo  $federico->nome; ?> <?php echo  $federico->cognome; ?> </h2>
-       <h2> prodotto per: <?php echo  $gomitolo->nome_animale; ?></h2>
-       <h3> cibo: <?php echo  $crocchette->nome_accessorio; ?> </h3>
-       <h3> costo: <?php echo  $crocchette->prezzo; ?> </h3>
+       <img class="two" src="img/crocchette.jpg" alt="">
+       <h2> Utente: <?php echo  $federico->nome; ?> <?php echo  $federico->cognome; ?> </h2>
+       <h2> Prodotto per: <?php echo  $gomitolo->nome_animale; ?></h2>
+       <h3> Cibo: <?php echo  $crocchette->nome_accessorio; ?> </h3>
+       <h3> Costo: <?php echo  $crocchette->prezzo; ?> </h3>
 
     </div>
     <div class="card">
-       <h2> utente: <?php echo  $matteo->nome; ?> <?php echo  $matteo->cognome; ?> </h2>
-       <h2> prodotto per: <?php echo  $peluque->nome_animale; ?></h2>
-       <h3> accessorio: <?php echo  $museruola->nome_accessorio; ?> </h3>
-       <h3> costo: <?php echo  $museruola->prezzo; ?> </h3>
+       <img class="m" src="img/mus.jpg" alt="">
+       <h2> Utente: <?php echo  $matteo->nome; ?> <?php echo  $matteo->cognome; ?> </h2>
+       <h2> Prodotto per: <?php echo  $peluque->nome_animale; ?></h2>
+       <h3> Accessorio: <?php echo  $museruola->nome_accessorio; ?> </h3>
+       <h3> Costo: <?php echo  $museruola->prezzo; ?> </h3>
     </div>
 </main>
 
